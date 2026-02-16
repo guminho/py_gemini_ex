48b500450512554a3912ef747d3fa490d240900f
hoangnm97 <hoangnm97s@gmail.com>
2026-02-17 02:17:50 +0700

    add thinks

---
+++ thinks/t1_sum.py
@@ -0,0 +1,20 @@
+from google import genai
+from google.genai import types
+
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview",
+    contents="What is the sum of the first 30 prime numbers?",
+    config=types.GenerateContentConfig(
+        thinking_config=types.ThinkingConfig(include_thoughts=True)
+    ),
+)
+for part in response.candidates[0].content.parts:
+    if not part.text:
+        continue
+    if part.thought:
+        print(f"Thought summary:\n{part.text}\n")
+    else:
+        print(f"Answer:\n{part.text}\n")
+print(response.usage_metadata.model_dump_json(indent=2))
---
+++ thinks/t2_level.py
@@ -0,0 +1,17 @@
+from google import genai
+from google.genai import types
+
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-3-flash-preview",
+    contents="What is the sum of the first 30 prime numbers?",
+    config=types.GenerateContentConfig(
+        thinking_config=types.ThinkingConfig(
+            thinking_level="minimal",
+            # thinking_budget=0,
+        )
+    ),
+)
+print(response.text)
+print(response.usage_metadata.model_dump_json(indent=2))
