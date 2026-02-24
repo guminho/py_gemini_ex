31cfbfdbdb32172d8dc9da3924e8ca9055965767
hoangnm97 <v.hoangnm24@vinit.tech>
2026-02-24 09:15:21 +0700

    add file part from uri

--- core/doc1_url_ext.py
+++ core/doc1_url_ext.py
@@ -1,12 +1,10 @@
-import requests
 from google import genai
 from google.genai.types import Part
 
 client = genai.Client()
 
 url = "https://arxiv.org/pdf/1706.03762"
-data = requests.get(url).content
-doc = Part.from_bytes(data=data, mime_type="application/pdf")
+doc = Part.from_uri(file_uri=url, mime_type="application/pdf")
 response = client.models.generate_content(
     model="gemini-3-flash-preview",
     contents=[doc, "Summarize this document"],
