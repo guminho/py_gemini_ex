0b81784e4495af5cde96ed03322fe6dc996ef88f
hoangnm97 <v.hoangnm24@vinit.tech>
2026-02-27 16:09:02 +0700

    add adk gemini adv config

--- basic_weather/agent.py
+++ basic_weather/agent.py
@@ -1,13 +1,25 @@
 from google.adk.agents import LlmAgent
+from google.adk.models.google_llm import Gemini
+from google.adk.planners import BuiltInPlanner
+from google.genai import types
 
 from tools.tools import get_current_time, get_weather
 
 root_agent = LlmAgent(
     name="weather_time_agent",
-    model="gemini-3-flash-preview",
+    model=Gemini(model="gemini-3-flash-preview"),
     description="Agent to answer questions about the time and weather in a city.",
     instruction=(
         "You are a helpful agent who can answer user questions about the time and weather in a city."
     ),
     tools=[get_weather, get_current_time],
+    generate_content_config=types.GenerateContentConfig(
+        temperature=1.0,
+    ),
+    planner=BuiltInPlanner(
+        thinking_config=types.ThinkingConfig(
+            thinking_level="high",
+            include_thoughts=True,
+        )
+    ),
 )
